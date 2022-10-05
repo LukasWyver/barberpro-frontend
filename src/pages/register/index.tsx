8fbc7afdcbbf957d4bb3daa0b4dcd8bbf47dba68
Lucas Gimenes <lucas_sin5@hotmail.com>
2022-10-05 00:38:23 -0300

    create context api

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import Head from "next/head";
 import Image from "next/image";
@@ -13,28 +13,23 @@ import {
   Text,
 } from "@chakra-ui/react";
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
-import { AuthContext } from "../../context/AuthContext";
 
-export default function Login() {
-  const { signIn } = useContext(AuthContext);
-
+export default function Register() {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const [show, setShow] = useState(false);
   const handleClick = () => setShow(!show);
 
-  async function handleLogin() {
-    await signIn({
-      email,
-      password,
-    });
+  function handleRegister() {
+    alert("teste");
   }
 
   return (
     <>
       <Head>
-        <title>BarberPRO - Faça login para acessar</title>
+        <title>Crie sua conta no BarberPRO</title>
       </Head>
 
       <Flex
@@ -56,14 +51,28 @@ export default function Login() {
           <Input
             mb={3}
             size="lg"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            color="white"
+            variant="filled"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            background="barber.400"
+            placeholder="Nome da barbearia"
+            focusBorderColor="barber.100"
+            _hover={{ bg: "barber.400" }}
+          />
+
+          <Input
+            size="lg"
             type="email"
             color="white"
             variant="filled"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             background="barber.400"
             placeholder="email@email.com"
             focusBorderColor="barber.100"
+            mb={3}
             _hover={{ bg: "barber.400" }}
           />
 
@@ -72,12 +81,12 @@ export default function Login() {
               pr="4.5rem"
               color="white"
               variant="filled"
-              background="barber.400"
-              focusBorderColor="barber.100"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+              background="barber.400"
+              focusBorderColor="barber.100"
               type={show ? "text" : "password"}
-              placeholder="Digite sua senha"
+              placeholder="Cadastrar uma senha"
               _hover={{ bg: "barber.400" }}
             />
             <InputRightElement width="4.5rem">
@@ -92,20 +101,20 @@ export default function Login() {
           </InputGroup>
 
           <Button
-            background="button.cta"
             mb={6}
-            color="gray.900"
             size="lg"
+            color="gray.900"
+            onClick={handleRegister}
+            background="button.cta"
             _hover={{ bg: "#ffb13e" }}
-            onClick={handleLogin}
           >
-            Acessar
+            Cadastrar
           </Button>
 
           <Center mt={2}>
-            <Link href="/register">
+            <Link href="/login">
               <Text color="white" cursor="pointer">
-                Ainda não possui conta? <strong>Cadastre-se</strong>
+                Já possui uma conta? <strong>Faça login</strong>
               </Text>
             </Link>
           </Center>
